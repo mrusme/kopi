5c685d4e4225e778e4d53b058ba42b66e74fd86b
マリウス <marius@xn--gckvb8fzb.com>
2024-08-06 19:54:52 -0500

    Add Methods, correct caffeine_multiplier_per_ml https://www.coffeeness.de/en/how-much-caffeine-in-coffee/

--- migrations/006_methods.up.sql
+++ migrations/006_methods.up.sql
@@ -21,8 +21,136 @@ INSERT INTO methods (
   "Espresso Maker",
   "A generic Espresso maker",
 
-  0.5,
+  2.73,
 
   TRUE
 );
+
+INSERT INTO methods (
+	id,
+	name,
+	description,
+
+	caffeine_multiplier_per_ml,
+
+	is_hot
+) VALUES (
+  "drip_coffee_maker",
+  "Drop Coffee Maker",
+  "A generic Drip Coffee maker",
+
+  0.68,
+
+  TRUE
+);
+
+INSERT INTO methods (
+	id,
+	name,
+	description,
+
+	caffeine_multiplier_per_ml,
+
+	is_hot
+) VALUES (
+  "pour_over",
+  "Pour Over",
+  "Pour Over",
+
+  0.74,
+
+  TRUE
+);
+
+
+INSERT INTO methods (
+	id,
+	name,
+	description,
+
+	caffeine_multiplier_per_ml,
+
+	is_hot
+) VALUES (
+  "french_press",
+  "French Press",
+  "A French Press coffee maker",
+
+  0.89,
+
+  TRUE
+);
+
+INSERT INTO methods (
+	id,
+	name,
+	description,
+
+	caffeine_multiplier_per_ml,
+
+	is_hot
+) VALUES (
+  "aeropress",
+  "AeroPress",
+  "An AeroPress coffee maker",
+
+  1.36,
+
+  TRUE
+);
+
+INSERT INTO methods (
+	id,
+	name,
+	description,
+
+	caffeine_multiplier_per_ml,
+
+	is_hot
+) VALUES (
+  "moka_pot",
+  "Moka Pot",
+  "A Moka pot",
+
+  1.64,
+
+  TRUE
+);
+
+INSERT INTO methods (
+	id,
+	name,
+	description,
+
+	caffeine_multiplier_per_ml,
+
+	is_hot
+) VALUES (
+  "cezve",
+  "Cezve",
+  "A Turkish Coffee Cezve",
+
+  1.5,
+
+  TRUE
+);
+
+INSERT INTO methods (
+	id,
+	name,
+	description,
+
+	caffeine_multiplier_per_ml,
+
+	is_hot
+) VALUES (
+  "cold_brew",
+  "Cold Brew",
+  "A Cold Brew",
+
+  1.12,
+
+  TRUE
+);
+
 
