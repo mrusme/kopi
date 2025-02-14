fbc9943886aacac985687daa06eb865818dbff56
マリウス <marius@xn--gckvb8fzb.com>
2025-02-13 19:50:12 -0500

    Add drinks

--- migrations/007_drinks.up.sql
+++ migrations/007_drinks.up.sql
@@ -37,12 +37,12 @@ INSERT INTO drinks (
 ) VALUES (
   "espresso",
   "Espresso",
-  "A regular Espresso",
+  "A single-shot Espresso",
 
-  9,
-  37,
-  37,
-  0,
+  9, -- TODO
+  25, -- TODO
+  25, -- TODO
+  0, -- TODO
   0,
 
   TRUE,
@@ -52,3 +52,256 @@ INSERT INTO drinks (
   "espresso_maker",
   "espresso_maker grinder"
 );
+
+INSERT INTO drinks (
+	id,
+	name,
+	description,
+
+	coffee_g,
+	brew_ml,
+	water_ml,
+	milk_ml,
+	sugar_g,
+
+	is_hot,
+	is_always_vegan,
+	can_be_vegan,
+
+  compatible_methods,
+  compatible_equipment
+) VALUES (
+  "cappuccino",
+  "Cappuccino",
+  "A single-shot Cappuccino",
+
+  9, -- TODO
+  25, -- TODO
+  25, -- TODO
+  100, -- TODO
+  0,
+
+  TRUE,
+  FALSE,
+  TRUE,
+
+  "espresso_maker",
+  "espresso_maker grinder frother"
+);
+
+INSERT INTO drinks (
+	id,
+	name,
+	description,
+
+	coffee_g,
+	brew_ml,
+	water_ml,
+	milk_ml,
+	sugar_g,
+
+	is_hot,
+	is_always_vegan,
+	can_be_vegan,
+
+  compatible_methods,
+  compatible_equipment
+) VALUES (
+  "latte",
+  "Latte",
+  "A single-shot Latte",
+
+  9, -- TODO
+  25, -- TODO
+  25, -- TODO
+  180, -- TODO
+  0,
+
+  TRUE,
+  FALSE,
+  TRUE,
+
+  "espresso_maker",
+  "espresso_maker grinder frother"
+);
+
+INSERT INTO drinks (
+	id,
+	name,
+	description,
+
+	coffee_g,
+	brew_ml,
+	water_ml,
+	milk_ml,
+	sugar_g,
+
+	is_hot,
+	is_always_vegan,
+	can_be_vegan,
+
+  compatible_methods,
+  compatible_equipment
+) VALUES (
+  "flat_white",
+  "Flat White",
+  "A single-shot Flat White",
+
+  9, -- TODO
+  25, -- TODO
+  25, -- TODO
+  180, -- TODO
+  0,
+
+  TRUE,
+  FALSE,
+  TRUE,
+
+  "espresso_maker",
+  "espresso_maker grinder frother"
+);
+
+INSERT INTO drinks (
+	id,
+	name,
+	description,
+
+	coffee_g,
+	brew_ml,
+	water_ml,
+	milk_ml,
+	sugar_g,
+
+	is_hot,
+	is_always_vegan,
+	can_be_vegan,
+
+  compatible_methods,
+  compatible_equipment
+) VALUES (
+  "macchiato",
+  "Macchiato",
+  "A single-shot Macchiato",
+
+  9, -- TODO
+  25, -- TODO
+  25, -- TODO
+  20, -- TODO
+  0,
+
+  TRUE,
+  FALSE,
+  TRUE,
+
+  "espresso_maker",
+  "espresso_maker grinder frother"
+);
+
+INSERT INTO drinks (
+	id,
+	name,
+	description,
+
+	coffee_g,
+	brew_ml,
+	water_ml,
+	milk_ml,
+	sugar_g,
+
+	is_hot,
+	is_always_vegan,
+	can_be_vegan,
+
+  compatible_methods,
+  compatible_equipment
+) VALUES (
+  "latte_macchiato",
+  "Latte Macchiato",
+  "A single-shot Latte Macchiato",
+
+  9, -- TODO
+  25, -- TODO
+  25, -- TODO
+  20, -- TODO
+  0,
+
+  TRUE,
+  FALSE,
+  TRUE,
+
+  "espresso_maker",
+  "espresso_maker grinder frother"
+);
+
+INSERT INTO drinks (
+	id,
+	name,
+	description,
+
+	coffee_g,
+	brew_ml,
+	water_ml,
+	milk_ml,
+	sugar_g,
+
+	is_hot,
+	is_always_vegan,
+	can_be_vegan,
+
+  compatible_methods,
+  compatible_equipment
+) VALUES (
+  "cortado",
+  "Cortado",
+  "A single-shot Cortado",
+
+  9, -- TODO
+  25, -- TODO
+  25, -- TODO
+  60, -- TODO
+  0,
+
+  TRUE,
+  FALSE,
+  TRUE,
+
+  "espresso_maker",
+  "espresso_maker grinder frother"
+);
+
+INSERT INTO drinks (
+	id,
+	name,
+	description,
+
+	coffee_g,
+	brew_ml,
+	water_ml,
+	milk_ml,
+	sugar_g,
+
+	is_hot,
+	is_always_vegan,
+	can_be_vegan,
+
+  compatible_methods,
+  compatible_equipment
+) VALUES (
+  "drip_coffee",
+  "Drip Coffee",
+  "A regular Drip Coffee",
+
+  10, -- TODO
+  180, -- TODO
+  180, -- TODO
+  0, -- TODO
+  0,
+
+  TRUE,
+  TRUE,
+  TRUE,
+
+  "drip_coffee_maker pour_over",
+  "coffee_maker grinder"
+);
+
